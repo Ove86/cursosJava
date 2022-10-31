a2bc1d9881875d4c283917555f8b5cbf46a0bba1
Ove <ser.ove@hotmail.com>
2022-10-31 09:16:59 +0100

    cambios nuevos

---
+++ 54-prestamos-react/src/App.js
@@ -0,0 +1,25 @@
+import React, {Fragment, useState} from 'react';
+import Header from './componentes/Header';
+import Formulario from './componentes/formulario';
+
+function App() {
+  //Definir el state
+  const [cantidad, guardarCantidad]= useState(0);
+
+
+  return (
+    <Fragment>
+      <Header
+      titulo="Cotizador de prestamos"
+      />
+      <div className="container">
+        <Formulario 
+          cantidad={cantidad}
+          guardarCantidad={guardarCantidad}
+          />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
