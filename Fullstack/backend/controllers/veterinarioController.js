6d07de73255f210197bbbc403a0fa93a3ce72b6a
Ove <ser.ove@hotmail.com>
2022-11-05 12:41:35 +0100

    backend finalizado

--- Fullstack/backend/controllers/veterinarioController.js
+++ Fullstack/backend/controllers/veterinarioController.js
@@ -1,3 +1,4 @@
+import generarId from '../helpers/generarId.js';
 import generarJWT from '../helpers/generarJWT.js';
 import Veterinario from '../models/Veterinario.js';
 
@@ -24,7 +25,9 @@ const registrar = async (req,res)=>{
 };
 
 const perfil = (req,res)=>{
-    res.json({msg:"Mostrando el perfil"});
+    const {veterinario} = req;
+
+    res.json({perfil:veterinario});
 };
 
 const confirmar = async (req,res)=>{
@@ -67,4 +70,54 @@ const autenticar = async (req,res) => {
     }
 };
 
-export {registrar, perfil, confirmar, autenticar};
+const olvidePassword = async (req, res) => {
+    const {email} = req.body;
+    const existeVeterinario = await Veterinario.findOne({email});
+    if (!existeVeterinario){
+        const error = new Error ('El usuario no existe');
+        return res.status(400).json({msg: error.message});
+    }
+    try {
+        existeVeterinario.token = generarId();
+        await existeVeterinario.save()
+        res.json({msg:'Hemos olvidado un email con las instrucciones.'});
+    } catch (error) {
+        console.log (error);
+    }
+};
+
+const comprobarToken = async (req, res) => {
+    const {token} = req.params;
+    const tokenValido = await Veterinario.findOne({token});
+    if(tokenValido){
+        //El token es valido
+        res.json({msg: "Token válido, el usuario existe"})
+    }else{
+        const error = new Error ('Token no valido');
+        return res.status(400).json({msg: error.message})
+    }
+    
+};
+
+const nuevoPassword = async (req, res) => {
+   const {token} = req.params;
+   const {password} = req.body;
+   
+   const veterinario = await Veterinario.findOne({token})
+
+   if (!veterinario){
+    const error = new Error ('El usuario no existe');
+    return res.status(400).json({msg: error.message});
+    }
+
+    try {
+        veterinario.token = null;
+        veterinario.password = password;
+        await veterinario.save();
+        res.json({msg: 'Password modificado correctamente'})
+    } catch (error) {
+        console.log (error);
+    }
+};
+
+export {registrar, perfil, confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword};
--- Fullstack/backend/routes/veterinarioRoutes.js
+++ Fullstack/backend/routes/veterinarioRoutes.js
@@ -1,13 +1,17 @@
 import express from 'express';
-import { perfil, registrar,confirmar, autenticar } from '../controllers/VeterinarioController.js';
+import { perfil, registrar,confirmar, autenticar, olvidePassword, comprobarToken, nuevoPassword } from '../controllers/VeterinarioController.js';
 import checkAuth from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-
+// lo pueden ver todo el mundo
 router.post("/", registrar);
 router.get("/confirmar/:token",confirmar);
 router.post("/login",autenticar);
+router.post('olvide-password', olvidePassword);
+router.get('olvide-password/:token', comprobarToken);
+router.post('olvide-password/:token', nuevoPassword);
 
+// solo lo ven usuarios registrados
 router.get("/perfil",checkAuth, perfil);
 
 export default router;
